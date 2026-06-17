275909f7a1cdb3f0ebb394f3c1c5bf12760ffc91
moon97a <moon97ji@gmail.com>
2026-06-17 10:45:43 +0900

    csv업로드디버깅

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -10,4 +10,4 @@ const fileFilter = (req, file, cb) => {
   else cb(new Error('지원하지 않는 파일 형식입니다.'), false);
 };
 
-export default multer({ storage, fileFilter, limits: { fileSize: 50 * 1024 * 1024 } });
+export default multer({ storage, fileFilter, limits: { fileSize: 100 * 1024 * 1024 } });
